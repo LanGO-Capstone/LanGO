8d4827452ed200529891280bb556e42c3c374185
Kevin Arnall <kevinjarnall@gmail.com>
2019-11-16 11:40:50 -0600

    half a seeder file lol

---
+++ seeder.sql
@@ -0,0 +1,175 @@
+use lango_db;
+
+insert into languages (language)
+values ('Hungarian');
+insert into languages (language)
+values ('Hiri Motu');
+insert into languages (language)
+values ('Thai');
+insert into languages (language)
+values ('Lithuanian');
+insert into languages (language)
+values ('Georgian');
+insert into languages (language)
+values ('Aymara');
+insert into languages (language)
+values ('Dutch');
+insert into languages (language)
+values ('Croatian');
+insert into languages (language)
+values ('Amharic');
+insert into languages (language)
+values ('Swati');
+insert into languages (language)
+values ('Kashmiri');
+insert into languages (language)
+values ('Luxembourgish');
+insert into languages (language)
+values ('Afrikaans');
+insert into languages (language)
+values ('Azeri');
+insert into languages (language)
+values ('Swahili');
+insert into languages (language)
+values ('Tetum');
+insert into languages (language)
+values ('Papiamento');
+insert into languages (language)
+values ('Kannada');
+insert into languages (language)
+values ('German');
+insert into languages (language)
+values ('English');
+
+insert into users (email, password)
+values ('medward0@disqus.com', 'mFZcaav0HO');
+insert into users (email, password)
+values ('lbarry1@businessweek.com', 'arokCOL');
+insert into users (email, password)
+values ('tsteventon2@nytimes.com', 'VdtUrs');
+insert into users (email, password)
+values ('amcmeekin3@lycos.com', 'kLk0sQo');
+insert into users (email, password)
+values ('fsonghurst4@comcast.net', 'zFgjDzAf5');
+insert into users (email, password)
+values ('cfarquarson5@adobe.com', '4sR9UM');
+insert into users (email, password)
+values ('lpatnelli6@php.net', 'P6gHTtUSpB');
+insert into users (email, password)
+values ('bmalec7@de.vu', '5Urcz5T14zY');
+insert into users (email, password)
+values ('csouley8@desdev.cn', 'InayF5jKFr');
+insert into users (email, password)
+values ('pchinn9@globo.com', 'gnjcSyv1');
+insert into users (email, password)
+values ('tlushera@deviantart.com', 'GmYco6xTa78');
+insert into users (email, password)
+values ('lproverb@yellowbook.com', 'tZYIHyZ9CKfN');
+insert into users (email, password)
+values ('bfetherbyc@alibaba.com', 'ysynec');
+insert into users (email, password)
+values ('ncobbed@de.vu', 'h4KLCx6v');
+insert into users (email, password)
+values ('mbrumhame@yellowpages.com', 'BnGVtmvX');
+insert into users (email, password)
+values ('oiapicof@sourceforge.net', 'KkfT46m');
+insert into users (email, password)
+values ('lpietronig@ebay.com', '7sOY1R');
+insert into users (email, password)
+values ('adumelowh@abc.net.au', 'M41hPR2ymeXM');
+insert into users (email, password)
+values ('gfigiovannii@cbc.ca', 'W1ywHGl');
+insert into users (email, password)
+values ('lpowersj@merriam-webster.com', 'TjFMb8JWg8Sw');
+
+insert into images (url) values ('https://robohash.org/voluptassaepenon.bmp?size=50x50&set=set1');
+insert into images (url) values ('https://robohash.org/dolorumrepellatet.jpg?size=50x50&set=set1');
+insert into images (url) values ('https://robohash.org/nammolestiaequidem.bmp?size=50x50&set=set1');
+insert into images (url) values ('https://robohash.org/aliquidautemsit.bmp?size=50x50&set=set1');
+insert into images (url) values ('https://robohash.org/autisteenim.bmp?size=50x50&set=set1');
+insert into images (url) values ('https://robohash.org/utrepellatet.png?size=50x50&set=set1');
+insert into images (url) values ('https://robohash.org/rerumexpeditareiciendis.bmp?size=50x50&set=set1');
+insert into images (url) values ('https://robohash.org/necessitatibussintenim.jpg?size=50x50&set=set1');
+insert into images (url) values ('https://robohash.org/ipsumlaboreeum.bmp?size=50x50&set=set1');
+insert into images (url) values ('https://robohash.org/reiciendiseaqueest.png?size=50x50&set=set1');
+insert into images (url) values ('https://robohash.org/etdeseruntnatus.jpg?size=50x50&set=set1');
+insert into images (url) values ('https://robohash.org/accusamuseiusaut.png?size=50x50&set=set1');
+insert into images (url) values ('https://robohash.org/inventoreprovidentdolor.jpg?size=50x50&set=set1');
+insert into images (url) values ('https://robohash.org/nameumcum.png?size=50x50&set=set1');
+insert into images (url) values ('https://robohash.org/eautprovident.png?size=50x50&set=set1');
+insert into images (url) values ('https://robohash.org/quipossimuset.bmp?size=50x50&set=set1');
+insert into images (url) values ('https://robohash.org/delectusaliquamaut.jpg?size=50x50&set=set1');
+insert into images (url) values ('https://robohash.org/reiciendisnumquameaque.jpg?size=50x50&set=set1');
+insert into images (url) values ('https://robohash.org/utcorruptidelectus.png?size=50x50&set=set1');
+insert into images (url) values ('https://robohash.org/voluptasrerumnobis.bmp?size=50x50&set=set1');
+
+insert into userdetails (user_id, about_me, display_name, interests, join_date, location, profile_image_id) values (1, 'Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.
+
+In congue. Etiam justo. Etiam pretium iaculis justo.', 'wstoffler0', 'User-centric well-modulated toolset', '2019-03-04 22:48:17', 'Portugal', 1);
+insert into userdetails (user_id, about_me, display_name, interests, join_date, location, profile_image_id) values (2, 'Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.
+
+Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.
+
+Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', 'cgoodall1', 'Optional real-time orchestration', '2019-01-24 17:02:37', 'Russia', 2);
+insert into userdetails (user_id, about_me, display_name, interests, join_date, location, profile_image_id) values (3, 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.
+
+Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.
+
+Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.', 'vwithrington2', 'Function-based disintermediate emulation', '2019-04-30 17:58:12', 'France', 3);
+insert into userdetails (user_id, about_me, display_name, interests, join_date, location, profile_image_id) values (4, 'Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.
+
+Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.
+
+Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.', 'gbellon3', 'Advanced national artificial intelligence', '2019-02-12 01:56:30', 'China', 4);
+insert into userdetails (user_id, about_me, display_name, interests, join_date, location, profile_image_id) values (5, 'Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.
+
+Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.
+
+Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.', 'tcampelli4', 'Realigned exuding definition', '2019-03-05 16:06:39', 'Ethiopia', 5);
+insert into userdetails (user_id, about_me, display_name, interests, join_date, location, profile_image_id) values (6, 'Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.
+
+Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.', 'aathey5', 'Integrated clear-thinking Graphic Interface', '2019-05-23 11:19:13', 'Russia', 6);
+insert into userdetails (user_id, about_me, display_name, interests, join_date, location, profile_image_id) values (7, 'Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.
+
+Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.', 'fingraham6', 'Integrated clear-thinking data-warehouse', '2019-01-12 23:40:52', 'Czech Republic', 7);
+insert into userdetails (user_id, about_me, display_name, interests, join_date, location, profile_image_id) values (8, 'Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.', 'pskillings7', 'Fundamental web-enabled protocol', '2019-03-21 17:06:39', 'France', 8);
+insert into userdetails (user_id, about_me, display_name, interests, join_date, location, profile_image_id) values (9, 'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.
+
+Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.
+
+Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.', 'rcuolahan8', 'Centralized leading edge open system', '2019-05-13 10:48:02', 'Russia', 9);
+insert into userdetails (user_id, about_me, display_name, interests, join_date, location, profile_image_id) values (10, 'Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.
+
+Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.
+
+Fusce consequat. Nulla nisl. Nunc nisl.', 'kmoakes9', 'Upgradable empowering moderator', '2019-03-11 22:49:41', 'China', 10);
+insert into userdetails (user_id, about_me, display_name, interests, join_date, location, profile_image_id) values (11, 'Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.
+
+Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.', 'kwycliffea', 'Multi-tiered real-time synergy', '2019-07-24 19:19:10', 'Brazil', 11);
+insert into userdetails (user_id, about_me, display_name, interests, join_date, location, profile_image_id) values (12, 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.', 'fyersinb', 'Horizontal national collaboration', '2019-09-24 17:59:13', 'Ukraine', 12);
+insert into userdetails (user_id, about_me, display_name, interests, join_date, location, profile_image_id) values (13, 'Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.', 'clongthornec', 'Exclusive transitional benchmark', '2019-01-30 00:52:26', 'Nigeria', 13);
+insert into userdetails (user_id, about_me, display_name, interests, join_date, location, profile_image_id) values (14, 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.
+
+Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.
+
+In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.', 'lnusseyd', 'Grass-roots discrete project', '2019-02-20 09:04:53', 'China', 14);
+insert into userdetails (user_id, about_me, display_name, interests, join_date, location, profile_image_id) values (15, 'Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.
+
+Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.
+
+Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.', 'gkeenane', 'Configurable homogeneous encoding', '2018-12-05 01:10:53', 'China', 15);
+insert into userdetails (user_id, about_me, display_name, interests, join_date, location, profile_image_id) values (16, 'Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.
+
+Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.', 'sdibbinf', 'Switchable context-sensitive moratorium', '2019-06-28 01:47:37', 'Peru', 16);
+insert into userdetails (user_id, about_me, display_name, interests, join_date, location, profile_image_id) values (17, 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.
+
+Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', 'slisciardellig', 'Open-source radical protocol', '2019-05-21 22:49:07', 'Philippines', 17);
+insert into userdetails (user_id, about_me, display_name, interests, join_date, location, profile_image_id) values (18, 'Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.
+
+Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.', 'ahiomh', 'Operative multi-tasking toolset', '2019-10-29 06:55:41', 'China', 18);
+insert into userdetails (user_id, about_me, display_name, interests, join_date, location, profile_image_id) values (19, 'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.
+
+Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.', 'cbratchelli', 'Streamlined 4th generation toolset', '2019-07-18 17:14:07', 'Poland', 19);
+insert into userdetails (user_id, about_me, display_name, interests, join_date, location, profile_image_id) values (20, 'Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.
+
+Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.', 'brustedgej', 'Streamlined system-worthy projection', '2019-10-05 10:53:49', 'Ukraine', 20);
