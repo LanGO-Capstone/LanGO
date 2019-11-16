a4048d9adc665f53c123b1df9863f484b4a6a6fc
Kevin Arnall <kevinjarnall@gmail.com>
2019-11-16 07:21:57 -0600

    init commit

---
+++ src/main/java/com/codeup/lango/LanGoApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.lango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LanGoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/lango/LanGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.lango;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
