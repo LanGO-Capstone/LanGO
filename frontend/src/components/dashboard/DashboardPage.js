54deaa678a54bd979dccc231fb0cdd42572664f1
Kevin Arnall <kevinjarnall@gmail.com>
2019-11-18 11:16:55 -0600

    Nav menu working

--- frontend/src/components/dashboard/DashboardPage.js
+++ frontend/src/components/dashboard/DashboardPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import axios from 'axios';
+import {Link, Route, Switch} from 'react-router-dom';
 import OpportunityListItem from "../opportunities/OpportunityListItem";
 
 class DashboardPage extends React.Component {
 
     state = {
-        opportunities: []
+        opportunities: [],
+        activeTab: 0
     };
 
     componentDidMount() {
@@ -22,11 +24,65 @@ class DashboardPage extends React.Component {
             });
     }
 
+    changeTab = (index) => {
+        this.setState({
+            activeTab: index
+        });
+    };
+
     render() {
         return (
             <div className="container">
-                <h1>Dashboard</h1>
-                <ul className="list-unstyled">{this.state.opportunities}</ul>
+                <h1 className={"text-center"}>Dashboard</h1>
+                <ul className="nav nav-tabs">
+                    <li className="nav-item">
+                        <Link
+                            to={"/dashboard/"}
+                            onClick={() => this.changeTab(0)}
+                            className={"nav-link" + (this.state.activeTab === 0 ? " active" : "")}>
+                            Opportunities
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link
+                            to={"/dashboard/myopportunities"}
+                            onClick={() => this.changeTab(1)}
+                            className={"nav-link" + (this.state.activeTab === 1 ? " active" : "")}>
+                            My Opportunities
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link
+                            to={"/dashboard/interestedin"}
+                            onClick={() => this.changeTab(2)}
+                            className={"nav-link" + (this.state.activeTab === 2 ? " active" : "")}>
+                            Opportunities I'm Interested In
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link
+                            to={"/dashboard/upcoming"}
+                            onClick={() => this.changeTab(3)}
+                            className={"nav-link" + (this.state.activeTab === 3 ? " active" : "")}>
+                            Upcoming Opportunities
+                        </Link>
+                    </li>
+                </ul>
+                <Switch>
+                    <Route path={"/dashboard/myopportunities"}>
+                        <h1>My Opportunities</h1>
+                    </Route>
+                    <Route path={"/dashboard/interestedin"}>
+                        <h1>Interested in</h1>
+                    </Route>
+                    <Route path={"/dashboard/upcoming"}>
+                        <h1>Upcoming</h1>
+                    </Route>
+                    <Route path={"/dashboard"}>
+                        <h1>Opportunities</h1>
+                        <ul className="list-unstyled">{this.state.opportunities}</ul>
+                    </Route>
+                </Switch>
             </div>
         )
     }
