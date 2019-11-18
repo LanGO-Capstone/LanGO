4ae70e75674c5721237b28e601171201faf8fa92
beverlyjaimes <beverly.jaimespuente@gmail.com>
2019-11-18 16:16:23 -0600

    need to add checkbox

--- frontend/src/components/RegisterScreen.js
+++ frontend/src/components/RegisterScreen.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import {Link} from "react-router-dom";
 
 
 class RegisterScreen extends React.Component {
+
+    // this.handleInputCHange =
+    //     this.handleInputCHange.bind(this);
+    //
+    // handleInputChange(event) {
+    //     const target = event.target;
+    //     const value = target.type === 'checkbox' ? target.checked
+    //         : target.value;
+    //     const name = target.name;
+    //
+    //     this.setState({
+    //         [name] : value
+    //     });
+
+
+
 
     render() {
         return (
@@ -11,16 +26,24 @@ class RegisterScreen extends React.Component {
                 Register
             </div>
             <form method={"post"} action={"/api/register"} >
+                <label htmlFor="email">E-mail</label>
                 <div>
-                    <input name={"email"} placeholder={"Email"}/>
+                    <input name={"email"} placeholder={"E-mail"}/>
                 </div>
+                <label htmlFor="password">Password</label>
                 <div>
                     <input type={"password"} name={"password"} placeholder={"Password"}/>
                 </div>
+                <label htmlFor="confirmpassword">Confirm Password</label>
                 <div>
                     <input type={"password"} name={"confirmpassword"} placeholder={"Confirm password"}/>
                 </div>
-                <button>Continue</button>
+                {/*<button>Continue</button>*/}
+                <label htmlFor="displayname">Display Name</label>
+                <div>
+                    <input type={"text"} name={"displayname"} placeholder={"Display name"}/>
+                </div>
+                <label htmlFor="mylanguages">My Languages</label>
             </form>
             </div>
         )
