de04b3294cb1522124b27b417356820c62977214
John Cadena <cadena.john.85@gmail.com>
2019-11-18 11:56:11 -0600

    create Profile view with navigable tabs. Known Bug: wrong tab displays when choosing from navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import DashboardPage from './components/dashboard/DashboardPage';
+import ProfilePage from './components/profile/ProfilePage';
 import NavbarLoggedOut from "./components/common/NavbarLoggedOut";
 import NavbarLoggedIn from "./components/common/NavbarLoggedIn";
 import {HashRouter, Route, Switch} from "react-router-dom";
@@ -15,6 +16,11 @@ class App extends React.Component {
                 <Switch>
                     <Route path={"/dashboard"}>
                         <DashboardPage/>
+                    </Route>
+                </Switch>
+                <Switch>
+                    <Route path={"/profile"}>
+                        <ProfilePage/>
                     </Route>
                 </Switch>
             </HashRouter>
