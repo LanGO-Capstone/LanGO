60b211cabe69a9fbd06246c659e638a2bd778a52
beverlyjaimes <beverly.jaimespuente@gmail.com>
2019-11-19 15:02:32 -0600

    checkboxes added

--- frontend/src/components/RegisterScreen.js
+++ frontend/src/components/RegisterScreen.js
@@ -1,25 +1,38 @@
 import React from 'react';
+import axios from "axios";
 
 
 class RegisterScreen extends React.Component {
 
-    // this.handleInputCHange =
-    //     this.handleInputCHange.bind(this);
-    //
-    // handleInputChange(event) {
-    //     const target = event.target;
-    //     const value = target.type === 'checkbox' ? target.checked
-    //         : target.value;
-    //     const name = target.name;
-    //
-    //     this.setState({
-    //         [name] : value
-    //     });
+     languages= ['Arabic', 'A.S.L','English', 'French', 'French', 'German', 'Japanese', 'Japanese', 'Korean', 'Spanish', 'Tagalog', 'Vietnamese'];
 
+      state = {
+          view:this.props.view,
+          languages: []
+    };
 
-
+      static getDerivedStateFromProps(props, languages) {
+              if (props.view !== languages.view) {
+                  return {
+                      view: props.view
+        }
+        }
+              return null;
+      }
 
     render() {
+
+        let languagesList = this.languages.map((language) => {
+            return(<li key={language}>
+                <label htmlFor={"language"}>
+                    <input
+                        type="checkbox"
+                        value={"language"}
+                        name={"language"}
+                    />
+                </label>
+                {language}</li>)
+        });
         return (
             <div>
             <div>
@@ -44,6 +57,8 @@ class RegisterScreen extends React.Component {
                     <input type={"text"} name={"displayname"} placeholder={"Display name"}/>
                 </div>
                 <label htmlFor="mylanguages">My Languages</label>
+                <ul className={"list-unstyled"}>{languagesList}</ul>
+
             </form>
             </div>
         )
