a1e11632263ee8d93358bdb54bc05355bc347b41
John Cadena <cadena.john.85@gmail.com>
2019-11-19 10:09:31 -0600

    create files for CreatedOpps and InterestedOpps components

---
+++ frontend/src/components/feeds/CreatedOpportunities.js
@@ -0,0 +1,52 @@
+// import React from 'react';
+// import axios from "axios";
+// import OpportunityCardItem from "../opportunities/OpportunityCardItem";
+// import OpportunityListItem from "../opportunities/OpportunityListItem";
+//
+// class CreatedOpportunities extends React.Component {
+//
+//     state = {
+//         view: this.props.view,
+//         opportunities: []
+//     };
+//
+//     static getDerivedStateFromProps(props, state) {
+//         if (props.view !== state.view) {
+//             return {
+//                 view: props.view
+//             }
+//         }
+//         return null;
+//     }
+//
+//     componentDidMount() {
+//         // Hard-coded userId of 19; replace with userId of logged-in user
+//         // axios.get('/api/opportunities/upcoming')
+//         //     .then(res => this.setState({opportunities: res.data}));
+//         // axios.get('/api/users/19/created')
+//         //     .then(res => {
+//         //         createdList = res.data.map(opportunity => {
+//         //             return (<OpportunityListItem key={opportunity.id} opportunity={opportunity}/>)
+//         //         });
+//         //         this.setState({created: createdList});
+//         //     });
+//     }
+//
+//     render() {
+//         if (this.state.view === 'list') {
+//             let opportunityList = this.state.opportunities.map((opportunity) => {
+//                 return (<OpportunityListItem key={opportunity.id} opportunity={opportunity}/>)
+//             });
+//
+//             return (<ul className="list-unstyled">{opportunityList}</ul>)
+//         } else {
+//             let opportunityList = this.state.opportunities.map((opportunity) => {
+//                 return (<OpportunityCardItem key={opportunity.id} opportunity={opportunity}/>)
+//             });
+//
+//             return (<div>{opportunityList}</div>)
+//         }
+//     }
+// }
+//
+// export default CreatedOpportunities;
