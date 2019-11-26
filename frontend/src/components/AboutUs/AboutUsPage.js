5c7f902fa74a54a6938eb5130f07d4afac98ab85
Kevin Arnall <kevinjarnall@gmail.com>
2019-11-25 19:07:03 -0600

    AboutUs refactor and cleanup

--- frontend/src/components/AboutUs/AboutUsPage.js
+++ frontend/src/components/AboutUs/AboutUsPage.js
@@ -1,48 +1,48 @@
 import * as React from "react";
-import {Link} from "react-router-dom";
-
-
-
-
 
 class AboutUsPage extends React.Component {
+
     render() {
         return (
-
-       <div>
-           <h1 className="mb-5 mt-5 text-center">About Us</h1>
+            <div className={"container"}>
+                <h1 className=" my-5 text-center">About Us</h1>
                 <div className="row">
-                    <div id="aboutContainer" className="my-auto mx-auto mb-5">
-                        <div className="row">
-                            <div className="card mb-2 mr-5 border-0" style={{width: '15rem'}}>
-                                <img className="rounded-circle z-depth-2" alt="100x100" src={require('../AboutUs/AboutUsImages/KevinA.jpg')}/>
-                                    <p className="mt-5 text-center">Lorem ipsum dolor sit amet, mucius graecis in sit, per liber phaedrum cu.
-                                        Offendit detraxit nomin</p>
+                    <div className="col">
+                        <div className="card border-0">
+                            <img className="rounded-circle" alt="Kevin" src={require('../AboutUs/AboutUsImages/KevinA.jpg')}/>
+                            <div className="card-body">
+                                <p className="mt-5 text-center">Lorem ipsum dolor sit amet, mucius graecis in sit, per liber phaedrum cu. Offendit detraxit nomin</p>
                             </div>
-
-                                <div className="card mb-2 mr-5 border-0" style={{width: '15rem'}}>
-                                    <img className="rounded-circle z-depth-2" src={require('../AboutUs/AboutUsImages/john.jpg')} alt={''}/>
-                                        <p className="mt-5 text-center">Lorem ipsum dolor sit amet, mucius graecis in sit, per liber phaedrum cu. Offendit detraxit nomin</p>
-                                </div>
-
-                                <div className="card mb-2 border-0" style={{width: '15rem'}}>
-                                    <img className="rounded-circle z-depth-2" src={require('../AboutUs/AboutUsImages/BeverlyJ.jpg')} alt={''}/>
-                                    <p className="mt-5 text-center">Lorem ipsum dolor sit amet, mucius graecis in sit, per liber phaedrum cu. Offendit detraxit nomin</p>
-                                </div>
-
-                                <div className="card mb-2 ml-5 border-0" style={{width: '15rem'}}>
-                                    <img className="rounded-circle z-depth-2" src={require('../AboutUs/AboutUsImages/Annette.jpg')}alt={''}/>
-                                    <p className="mt-5 text-center">Lorem ipsum dolor sit amet, mucius graecis in sit, per liber phaedrum cu. Offendit detraxit nomin</p>
-                                </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card border-0">
+                            <img className="rounded-circle" src={require('../AboutUs/AboutUsImages/john.jpg')} alt={'John'}/>
+                            <div className="card-body">
+                                <p className="mt-5 text-center">Lorem ipsum dolor sit amet, mucius graecis in sit, per liber phaedrum cu. Offendit detraxit nomin</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card border-0">
+                            <img className="rounded-circle" src={require('../AboutUs/AboutUsImages/BeverlyJ.jpg')} alt={'Bev'}/>
+                            <div className="card-body">
+                                <p className="mt-5 text-center">Lorem ipsum dolor sit amet, mucius graecis in sit, per liber phaedrum cu. Offendit detraxit nomin</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card border-0">
+                            <img className="rounded-circle" src={require('../AboutUs/AboutUsImages/Annette.jpg')} alt={'Annette'}/>
+                            <div className="card-body">
+                                <p className="mt-5 text-center">Lorem ipsum dolor sit amet, mucius graecis in sit, per liber phaedrum cu. Offendit detraxit nomin</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-       </div>
-
+            </div>
         );
     }
-
-
 }
 
 export default AboutUsPage;
